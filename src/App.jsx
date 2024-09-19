c942b8de18d99dd3746ac5d91f9a9b58ee1f1704
RamiroSorroza <ramirosorroza@mail.com>
2024-09-19 15:25:09 -0300

    Create form for Contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import NavBar from "./components/Navbar/NavBar.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 function App() {
 
   return (
     <>
       <NavBar/>
+      <Contact/>
     </>
   )
 }
