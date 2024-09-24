22563cff4613949e602d4aed8f66522d9c8ab83e
Diego Pizarro <110474808+diegoandrespizarro@users.noreply.github.com>
2024-09-24 20:41:24 -0300

    Refactor dashboard user component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import NavBar from "./components/Navbar/NavBar.jsx";
 import Slider from "./components/Slider/Slider.jsx";
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer.jsx";
 import CardsAbautUs from "./Pages/AboutUs/CardsAbautUs.jsx";
 import RollingCode from "./Pages/RollingCode/Rolling.jsx";
 import EventsSection from "./Pages/HomeCards/HomeCards.jsx";
+import UserDash from "./Pages/Dashboard/dashboard.user.jsx";
+import Event from "./Pages/Dashboard/dashboard.event.jsx";
 
 function App() {
   return (
@@ -29,6 +32,15 @@ function App() {
             <>
               <CardsAbautUs />
               <RollingCode />
+            </>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <Event/>
+              <UserDash/>
             </>
           }
         />
