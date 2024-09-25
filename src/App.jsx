092133d60a162392f8aea2d7b8b84b51bbeda2f5
Mariano-godoy <godoyjosemariano@hotmail.com>
2024-09-24 22:54:41 -0300

    install dep

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+/*import "./App.css";
 import NavBar from "./components/Navbar/NavBar.jsx";
 import Slider from "./components/Slider/Slider.jsx";
 // eslint-disable-next-line no-unused-vars
@@ -9,13 +9,10 @@ import Footer from "./components/Footer/Footer.jsx";
 import CardsAbautUs from "./Pages/AboutUs/CardsAbautUs.jsx";
 import RollingCode from "./Pages/RollingCode/Rolling.jsx";
 import EventsSection from "./Pages/HomeCards/HomeCards.jsx";
-<<<<<<< HEAD
-import MerchCards from './Pages/Merchandising/MerchCards.jsx';
-=======
 import ContactPage from "./Pages/Contact/Contact.jsx";
 import UserDash from "./Pages/Dashboard/Dashboard.user.jsx";
 import Event from "./Pages/Dashboard/Dashboard.event.jsx";
->>>>>>> 25b87fcb1524c4e7541338fccac51e8a62694bc0
+import MerchCards from './Merchandising/MerchCards.js';
 
 function App() {
   return (
@@ -42,9 +39,6 @@ function App() {
             
           }
         />
-<<<<<<< HEAD
-        <Route path="/merchandising" element={<MerchCards />} />
-=======
          <Route
           path="/contact"
           element={
@@ -65,7 +59,6 @@ function App() {
             </>
           }
         />
->>>>>>> 25b87fcb1524c4e7541338fccac51e8a62694bc0
       </Routes>
       
 
@@ -74,4 +67,79 @@ function App() {
   );
 }
 
+export default App;*/
+import "./App.css"; 
+import NavBar from "./components/Navbar/NavBar.jsx";
+import Slider from "./components/Slider/Slider.jsx";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer.jsx";
+import CardsAbautUs from "./Pages/AboutUs/CardsAbautUs.jsx";
+import RollingCode from "./Pages/RollingCode/Rolling.jsx";
+import EventsSection from "./Pages/HomeCards/HomeCards.jsx";
+import ContactPage from "./Pages/Contact/Contact.jsx";
+import UserDash from "./Pages/Dashboard/Dashboard.user.jsx";
+import Event from "./Pages/Dashboard/Dashboard.event.jsx";
+import MerchCards from './Pages/Merchandising/MerchCards.jsx';  // Importación de MerchCards
+
+function App() {
+  return (
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route
+          path=""
+          element={
+            <>
+              <Slider />
+              <EventsSection />
+            </>
+          }
+        />
+        <Route
+          path="/desarrolladores"
+          element={
+            <>
+              <CardsAbautUs />
+              <RollingCode />
+            </>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <>
+              <ContactPage />
+            </>
+          }
+        />
+        <Route
+          path="/dash"
+          element={
+            <>
+              <UserDash />
+              <Event />
+            </>
+          }
+        />
+        {/* Nueva ruta para MerchCards */}
+        <Route
+          path="/merch"
+          element={
+            <>
+              <MerchCards />
+            </>
+          }
+        />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
 export default App;
+
