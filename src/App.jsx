dca0e5882376f341e1e911c88dae1292fef4a938
Mariano-godoy <godoyjosemariano@hotmail.com>
2024-09-24 22:57:20 -0300

    remove coment

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,6 @@
-/*import "./App.css";
-import NavBar from "./components/Navbar/NavBar.jsx";
-import Slider from "./components/Slider/Slider.jsx";
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer/Footer.jsx";
-import CardsAbautUs from "./Pages/AboutUs/CardsAbautUs.jsx";
-import RollingCode from "./Pages/RollingCode/Rolling.jsx";
-import EventsSection from "./Pages/HomeCards/HomeCards.jsx";
-import ContactPage from "./Pages/Contact/Contact.jsx";
-import UserDash from "./Pages/Dashboard/Dashboard.user.jsx";
-import Event from "./Pages/Dashboard/Dashboard.event.jsx";
-import MerchCards from './Merchandising/MerchCards.js';
-
-function App() {
-  return (
-    <>
-      <NavBar />
-
-      <Routes>
-        <Route
-          path=""
-          element={
-            <>
-              <Slider />
-              <EventsSection />
-            </>
-          }
-        />
-        <Route
-          path="/desarrolladores"
-          element={
-            <>
-              <CardsAbautUs />
-              <RollingCode />
-            </>
-            
-          }
-        />
-         <Route
-          path="/contact"
-          element={
-            <>
-              <ContactPage
-               />
-            </>
-          }
-        />
-        <Route
-          path="/dash"
-          element={
-            <>
-              <UserDash
-               />
-               <Event
-               />
-            </>
-          }
-        />
-      </Routes>
-      
-
-      <Footer />
-    </>
-  );
-}
-
-export default App;*/
 import "./App.css"; 
 import NavBar from "./components/Navbar/NavBar.jsx";
 import Slider from "./components/Slider/Slider.jsx";
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from "react-router-dom";
@@ -82,7 +11,7 @@ import EventsSection from "./Pages/HomeCards/HomeCards.jsx";
 import ContactPage from "./Pages/Contact/Contact.jsx";
 import UserDash from "./Pages/Dashboard/Dashboard.user.jsx";
 import Event from "./Pages/Dashboard/Dashboard.event.jsx";
-import MerchCards from './Pages/Merchandising/MerchCards.jsx';  // Importación de MerchCards
+import MerchCards from './Pages/Merchandising/MerchCards.jsx'; 
 
 function App() {
   return (
